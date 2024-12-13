73956b8c21f85e2f8c41509b9c871a01417c8407
“EdwardAugustoPriante” <edvardpriante@gmail.com>
2024-12-12 21:16:03 -0300

    atualizaçao

--- js/script.js
+++ js/script.js
@@ -1,8 +1,17 @@
 const login = document.getElementById('nome');
-const senha = document.getElementById(password);
+const senha = document.getElementById('password');
+const errorMsg = document.getElementById('error');
 
 function validarUsuario()
 {
     console.log(login.value + senha.value);
+    if(login.value === "" || senha.value === "")
+    {
+        errorMsg.style.display = 'block';
+    } else {
+        errorMsg.style.display = 'none';
+    }
+
+    // window.location.href = '../view/cadastro.html';
 }
 
